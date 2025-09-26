df23a82bd816a7b8608e8181d2953772af33183c
koushik rajan <koushikkalai25@gmail.com>
2025-09-26 22:24:05 +0530

    Commit before Project section

--- index.js
+++
@@ -1,6 +0,0 @@
-const contactBtn = document.getElementById('contactBtn');
-const contactBox = document.getElementById('contactBox');
-
-contactBtn.addEventListener('click', () => {
-  contactBox.style.display = contactBox.style.display === 'none' ? 'block' : 'none';
-});
